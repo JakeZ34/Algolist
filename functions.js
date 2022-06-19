90df3b3c25683b1f9204cbbc92e4639ae57a9798
JakeZ34 <JzBaseball34@hotmail.com>
2022-06-19 01:29:33 -0700

    up

--- functions.js
+++ functions.js
@@ -35,3 +35,13 @@ function highFive(num) {
     }
 }
 // Predcited outcome - Nothing would print to console?
+
+for(var j = 1; j <= 15; j++){
+    if(j % 2 == 0){
+        j+=2;
+    }
+    else if(j % 3 == 0){
+        j++;
+    }
+    console.log(j);
+}
--- loops.js
+++ loops.js
@@ -11,7 +11,20 @@ for(let i=0; i<5; i++){
     console.log('num:', i, 'sum:', sum)
 }
 
-var num=2
-var otherNum=3
-var sum='num' + 'otherNum'
-console.log('sum')
+function add(num1, num2) {
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    return sum;
+}
+var result1 = add(3, 5);
+var result2 = add(4, 7);
+console.log(result1);
+console.log(result2);
+
+function sumNums(num1, num2) {
+    return num1+num2;
+}
+console.log(sumNums(2,10))
+console.log(sumNums(500,10))
